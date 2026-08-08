77ff26edbce1bbace15102a714453717af2a496c
Manoel <ghostnether28@gmail.com>
2026-08-07 21:38:04 -0300

    feat (exercises): incluir teste de ambiente manual e ex 01

---
+++ exercises/01-dataTypes.c
@@ -0,0 +1,28 @@
+// Neste exercicio a ideia é criar variáveis de diferentes tipos de dados, atribuir valores a elas e imprimir esses valores na tela.
+// Após imprimir os valores, o programa deve retornar 0 para indicar que terminou com sucesso ou 1 para indicar que houve algum erro durante a execução.
+
+#include <stdio.h>
+
+int main (void) { // cabeçalho da função principal, que é o ponto de entrada do programa
+
+    // Declaração e inicialização de variáveis de diferentes tipos de dados
+    int inteiro = 10; // variável do tipo inteiro
+    float decimal = 3.14; // variável do tipo float
+    char caractere = 'A'; // variável do tipo char
+    double grandeDecimal = 2.718281828459045; // variável do tipo double
+
+    // Impressão dos valores das variáveis na tela
+    printf("Valor do inteiro: %d\n", inteiro);
+    printf("Valor do decimal: %.2f\n", decimal);
+    printf("Valor do caractere: %c\n", caractere);
+    printf("Valor do grande decimal: %.15f\n", grandeDecimal);
+
+        return 0; // retorna 0 para indicar que o programa terminou com sucesso.
+}
+
+// Conceitos importantes:
+// 1. Tipos de dados: Em C, existem diferentes tipos de dados, como int, float, char e double, cada um com suas próprias características e usos.
+// 2. Declaração e inicialização de variáveis: As variáveis devem ser declaradas com um tipo específico antes de serem usadas, e podem ser inicializadas com um valor no momento da declaração.
+// 3. Função printf: A função printf é usada para imprimir valores na tela, e utiliza especificadores de formato (como %d, %.2f, %c) para indicar o tipo de dado que está sendo impresso.
+// 4. Retorno da função main: A função main deve retornar um valor inteiro, onde 0 indica que o programa terminou com sucesso e qualquer outro valor indica que houve algum erro durante a execução.
+// 5. Comentários: Comentários são usados para explicar o código e não são executados pelo compilador. Eles podem ser de linha única (//) ou de múltiplas linhas (/* */).   
---
+++ exercises/helloWorld.c
@@ -0,0 +1,12 @@
+// Neste arquivo, vaos apenas fazer um programa que imprime "Hello World!" na tela.
+// como objetivo de testar se o ambiente de desenvolvimento está funcionando corretamente para rodar
+// um programa em C.
+
+#include <stdio.h> // biblioteca padrão de entrada e saída
+
+int main (void) { // cabeçalho da função principal, que é o ponto de entrada do programa
+        
+        printf("Hello World!\n"); // imprime a mensagem "Hello World!" na tela, seguida de uma nova linha
+
+        return 0; // retorna 0 para indicar que o programa terminou com sucesso.
+}
