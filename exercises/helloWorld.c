996b20209eed8542b678afeb61f7c17a05759879
Manoel <ghostnether28@gmail.com>
2026-08-09 16:22:44 -0300

    doc (helloWorld): acrescenta line commit

--- exercises/helloWorld.c
+++ exercises/helloWorld.c
@@ -1,4 +1,4 @@
-// Neste arquivo, vaos apenas fazer um programa que imprime "Hello World!" na tela.
+// Neste arquivo, vamos apenas fazer um programa que imprime "Hello World!" na tela.
 // como objetivo de testar se o ambiente de desenvolvimento está funcionando corretamente para rodar
 // um programa em C.
 
@@ -10,3 +10,11 @@ int main (void) { // cabeçalho da função principal, que é o ponto de entrada
 
         return 0; // retorna 0 para indicar que o programa terminou com sucesso.
 }
+
+// Este é um comentário de uma única linha, 
+//ele é ignorado pelo compiladore não afeta a execução do programa.
+
+/* Este espaço é reservado para comentários de multiplas linhas, eles são ignorados pelo compilador
+ou seja, não afetam a execução do programa. Eles são usados para documentar o código, explicar a lógica
+ou fornecer informações adicionais sobre o programa.
+*/
